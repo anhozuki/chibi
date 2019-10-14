06c6583120986e41ce0a0c648c3bf8cd3ceba3ab
OTA Mai <21716024@jwu.ac.jp>
2019-10-14 16:17:13 +0900

    1014

--- exp.py
+++ exp.py
@@ -30,3 +30,31 @@ print(e.eval())
 e = Add(Val(1),Add(Val(2),Val(3)))
 assert e.eval() == 6
 print(e.eval())
+
+class Mul(object):
+    __slots__=['left','right']
+    def __init__(self, a, b):
+        self.left = a
+        self.right = b
+    def eval(self):
+        return self.left.eval() * self.right.eval()
+
+e = Mul(Val(1),Val(2))
+print(e.eval())
+assert e.eval() == 2
+
+class Sub(object):
+    __slots__=['left','right']
+    def __init__(self, a, b):
+        self.left = a
+        self.right = b
+    def eval(self):
+        return self.left.eval() - self.right.eval()
+
+e = Sub(Val(2),Val(1))
+
+print(e.eval())
+assert e.eval() == 1
+
+print()
+print()
