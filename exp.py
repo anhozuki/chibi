8da133297f20e16c27d41d4364025a0b414d6606
OTA Mai <21716024@jwu.ac.jp>
2019-10-28 16:52:47 +0900

    1028

--- exp.py
+++ exp.py
@@ -1,104 +1,72 @@
 class Expr(object):
-   pass
+    pass
+
 class Val(Expr):
-   __slots__=['value']
-   def __init__(self, v = 0):
-       self.value = v
-   def __repr__(self):
-       return f'Val({self.value})'
-   def eval(self):
-       return self.value
+    __slots__ = ['value']
+    def __init__(self, value = 0):
+        self.value = value
+    def __repr__(self):
+        return f'Val({self.value})'
+    def eval(self):
+        return self.value
+
 v = Val(1)
-print(v)
+#print(v)
 assert v.eval() == 1
-class Add(Expr):
-   __slots__ = ['left', 'right']
-   def __init__(self, a, b):
-       self.left = a
-       self.right = b
-   def eval(self):
-       return self.left.eval() + self.right.eval()
-e = Add(Val(1),Val(2))
-print(e.eval())
-assert e.eval() == 3
-assert isinstance(v, Expr)
-assert isinstance(v, Val)
-assert  not isinstance(v, int)
+
 def toExpr(a):
-   if not isinstance(a, Expr):
-       a = Val(a)
-   return a
-class Add(Expr):
-   __slots__ = ['left', 'right']
-   def __init__(self, a, b):
-       self.left = toExpr(a)
-       self.right = toExpr(b)
-   def eval(self):
-       return self.left.eval() + self.right.eval()
-e = Add(1,Add(1, 2))
-print(e.eval())
+    if not isinstance(a, Expr):
+            a = Val(a)
+    return a
+
+class Binary(Expr):
+    def __repr__(self):
+        cname = self.__class__.__name__
+        return f'{cname}({self.left},{self.right})'
+
+class Add(Binary):
+    __slots__=['left', 'right']
+    def __init__(self, a, b):
+        self.left = toExpr(a)   # aとb は式
+        self.right = toExpr(b)
+    def eval(self):
+        return self.left.eval() + self.right.eval()
+
+e = Add(1,Add(1,2))
+#print(e)
 assert e.eval() == 4
 
-def __repr__(self):
-    classname = self.__class__.__name__
-    return f'{classname}({self.left},{self.right})'
-    
-class Val(object):
-   __slots__=['value']
-   def __init__(self, v = 0):
-       self.value = v
-   def __repr__(self):
-       return f'Val({self.value})'
-   def eval(self):
-       return self.value
-v = Val(1)
-print(v)
-assert v.eval() == 1
-class Add(object):
-   __slots__ = ['left', 'right']
-   def __init__(self, a, b):
-       self.left = a
-       self.right = b
-   def eval(self):
-       return self.left.eval() + self.right.eval()
-e = Add(Val(1), Val(2))
-print(e.eval())
-assert e.eval() == 3
-e = Add(Val(1), Add(Val(2), Val(3)))
-print(e.eval())
+e = Add(Val(1),Add(Val(2),Val(3)))
 assert e.eval() == 6
-class Mul(object):
-   __slots__ = ['left', 'right']
-   def __init__(self, a, b):
-       self.left = a
-       self.right = b
-   def eval(self):
-       return self.left.eval() * self.right.eval()
-e = Mul(Val(1), Val(2))
-print(e.eval())
-assert e.eval() == 2
-class Sub(object):
-   __slots__ = ['left', 'right']
-   def __init__(self, a, b):
-       self.left = a
-       self.right = b
-   def eval(self):
-       return self.left.eval() - self.right.eval()
-e = Sub(Val(1), Val(2))
-print(e.eval())
-assert e.eval() == -1
-class Div(object):
-   __slots__ = ['left', 'right']
-   def __init__(self, a, b):
-       self.left = a
-       self.right = b
-   def eval(self):
-       return self.left.eval() / self.right.eval()
-e = Div(Val(7), Val(2))
-print(e.eval())
-assert e.eval() == 3.5
 
-print()
-print()
+class Sub(Binary):
+    __slots__=['left', 'right']
+    def __init__(self, a, b):
+        self.left = toExpr(a)   # aとb は式
+        self.right = toExpr(b)
+    def eval(self):
+        return self.left.eval() - self.right.eval()
+
+class Mul(Binary):
+    __slots__=['left', 'right']
+    def __init__(self, a, b):
+        self.left = toExpr(a)   # aとb は式
+        self.right = toExpr(b)
+    def eval(self):
+        return self.left.eval() * self.right.eval()
+
+class Div(Binary):
+    __slots__=['left', 'right']
+    def __init__(self, a, b):
+        self.left = toExpr(a)   # aとb は式
+        self.right = toExpr(b)
+    def eval(self):
+        return self.left.eval() // self.right.eval()
+
+
+
+
+
+#数値計算法２ へのメッセージ
 
 
--- pasar.py
+++ pasar.py
@@ -1,16 +1,30 @@
-from exp import Val, Add
-
+from expslack import Val, Add, Sub, Mul, Div
 def parse(s: str):
-    num = int(s)
-    return Val(num)
-
+   num = int(s)
+   return Val(num)
+'''
 e = parse("123")
 print(e)
-
 s = "123+456"
-pos = s.find('+')
-print('pos',pos)
-
+pos = s.find('+') #+記号を探す
+print('pos', pos)
 s1 = s[0:pos]
 s2 = s[pos+1:]
-print(s,s1,s2)
+print(s, s1, s2) #+記号で分割
+'''
+def parse(s: str):
+   if s.find('+') > 0:
+       pos = s.find('+')
+       s1 = s[0:pos]
+       s2 = s[pos+1:]
+       return Add(parse(s1),parse(s2))
+   if s.find('*') > 0:
+       pos = s.find('*')
+       s1 = s[0:pos]
+       s2 = s[pos+1:]
+       return Mul(parse(s1),parse(s2))
+   return Val(int(s))
+e = parse("1*2+3")
+print(e, e.eval())
+e = parse("1+2*3")
+print(e, e.eval())
